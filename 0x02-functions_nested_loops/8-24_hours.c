c6a8cf31d907ad82e475bb42fb4a3c4b50a4b594
Ataminyo567 <ataminyoe@gmail.com>
2022-10-19 12:15:51 -0700

    My real commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of
- * Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer - Prints every minute of the day
  */
 void jack_bauer(void)
-}
+{
 int hour, minute;
 
 for (hour = 0; hour <= 23; hour++)
